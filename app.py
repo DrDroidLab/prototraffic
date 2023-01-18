5f56667ea1f68affa5dfe14f54cc6af6beb08b76
Dipesh Mittal <dipesh.mittal@shadowfax.in>
2023-01-17 17:15:13 -0800

    Removed city as group key

--- app.py
+++ app.py
@@ -55,18 +55,19 @@ def PlaceOrder():
 		order_id = request.form.get('order_id')
 
 		dr.publish("OrderAllocation", "Initiated", (('order_id', order_id), ('city', city), ('merchant_id', merchant_id), ('order_value', order_value), ('pickup_location', pickup_location), ('drop_location', drop_location)))
+		time.sleep(1)
 
 		drivers = getDrivers(city, pickup_location)
 
-		dr.publish("OrderAllocation", "DriverFetched", (('order_id', order_id), ('city', city), ('drivers_count', len(drivers))))
+		dr.publish("OrderAllocation", "DriverFetched", (('order_id', order_id), ('drivers_count', len(drivers))))
 
 		driver_id = allocateDriverToOrderNow(city, drivers)
 		if driver_id:
 			eta = getETAByDriverId(driver_id, drivers)
-			dr.publish("OrderAllocation", "AllocationSucceeded", (('order_id', order_id), ('city', city), ('driver_id', driver_id), ('eta', eta)))
+			dr.publish("OrderAllocation", "AllocationSucceeded", (('order_id', order_id), ('driver_id', driver_id), ('eta', eta)))
 			return {"order_allocated": True, 'eta_for_delivery': eta}	
 
-		dr.publish("OrderAllocation", "AllocationFailed", (('order_id', order_id), ('city', city), ('driver_id', driver_id)))
+		dr.publish("OrderAllocation", "AllocationFailed", (('order_id', order_id), ('driver_id', driver_id)))
 		return {"order_allocated": False, 'eta_for_delivery': None}	
 		
 
